ee84e67fd01fe3286a1d3416959c76b7f827ff8a
Shahid Shaikh <shahid.anjum.shaikh@gmail.com>
2026-07-07 00:39:57 +0100

    resized the buttons and added logos

--- customised-rackets/src/components/brand-buttons.jsx
+++ customised-rackets/src/components/brand-buttons.jsx
@@ -1,10 +1,28 @@
+import { useState } from 'react'
+import yonex from '../assets/yonexlogo.jpg';
+import victor from '../assets/victorlogo.jpg';
+import lining from '../assets/lininglogo.jpg';
+import apacs from '../assets/apacslogo.jpeg';
+
+
 export default function brandButtons() {
+    const [selected, setSelected] = useState(null)
+    
     return (
         <div className="brand-grid">
-            <button className="brand-button">Yonex</button>
-            <button className="brand-button">Victor</button>
-            <button className="brand-button">Li Ning</button>
-            <button className="brand-button">Apacs</button>
+            <button className="brand-button" onClick={() => setSelected('Yonex')}>
+                <img src={yonex} alt="Yonex" className={selected === 'Yonex' ? 'enlarged' : ''} />
+
+            </button>
+            <button className="brand-button" onClick={() => setSelected('Victor')}>
+                <img src={victor} alt="Victor" className={selected === 'Victor' ? 'enlarged' : ''} />
+            </button>
+            <button className="brand-button" onClick={() => setSelected('Li Ning')}>
+                <img src={lining} alt="Li Ning" className={selected === 'Li Ning' ? 'enlarged' : ''} />
+            </button>
+            <button className="brand-button" onClick={() => setSelected('Apacs')}>
+                <img src={apacs} alt="Apacs" className={selected === 'Apacs' ? 'enlarged' : ''} />
+            </button>
 
         </div>
     )
