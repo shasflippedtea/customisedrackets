c98f20f9659fc339b5c85de063fb4dd28451a4b5
Shahid Shaikh <shahid.anjum.shaikh@gmail.com>
2026-07-06 01:27:40 +0100

    customrackets first buttons

---
+++ customised-rackets/src/App.jsx
@@ -0,0 +1,8 @@
+import './App.css'
+import brandButtons from './components/brand-buttons'
+
+function App() {
+  return <brandButtons />
+}
+
+export default brandButtons
---
+++ customised-rackets/src/components/brand-buttons.jsx
@@ -0,0 +1,11 @@
+export default function brandButtons() {
+    return (
+        <div className="brand-grid">
+            <button className="brand-button">Yonex</button>
+            <button className="brand-button">Victor</button>
+            <button className="brand-button">Li Ning</button>
+            <button className="brand-button">Apacs</button>
+
+        </div>
+    )
+}
