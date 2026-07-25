68fc583194fc915f6a81c050982fcebfe282430e
shasflippedtea <shahid.anjum.shaikh@gmail.com>
2026-07-26 00:11:10 +0100

    implementing three.js

--- customised-rackets/src/main.jsx
+++ customised-rackets/src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
