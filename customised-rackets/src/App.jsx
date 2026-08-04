c287a75952c903d58a3e7ae2c1042dd2e107f9d1
shasflippedtea <shahid.anjum.shaikh@gmail.com>
2026-08-04 23:26:53 +0100

    Implemented mouse ctrl on the render using orbitcontrol, fixed the scaling

--- customised-rackets/src/App.jsx
+++ customised-rackets/src/App.jsx
@@ -4,7 +4,7 @@ import RacketViewer from './components/racketsviewer'
 
 function App() {
   return (
-  <main style={{ display: 'flex', flexDirection: 'row'}}>
+  <main style={{ display: 'flex', flexDirection: 'row', overflow: 'hidden', aligntItems: 'center', justifyContent: 'center', height: '100vh' }}>
     <BrandButtons />
     <RacketViewer />
   </main>
--- customised-rackets/src/components/racketsviewer.jsx
+++ customised-rackets/src/components/racketsviewer.jsx
@@ -2,34 +2,52 @@ import react from 'react';
 import { useEffect, useRef } from 'react';
 import * as THREE from 'three';
 import {OBJLoader} from 'three/addons/loaders/OBJLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 function RacketViewer() {
     const mountRef = useRef(null);
     
     useEffect(() => {
+
+        if (mountRef.current.children.length > 0) return; 
+
         const scene = new THREE.Scene();
+
         const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000,);
         camera.position.z = 10;
+
+
         const renderer = new THREE.WebGLRenderer(); // sizing the render 
-        renderer.setSize(window.innerWidth / 2, window.innerHeight);
+        renderer.setSize(mountRef.current.clientWidth, mountRef.current.clientHeight);
         mountRef.current.appendChild(renderer.domElement);
-        renderer.render(scene, camera);
+        renderer.setClearColor(0x000000, 0); // Transparent background
+
+        const controls = new OrbitControls(camera, renderer.domElement);
+
         const loader = new OBJLoader(); // To load the model file
-        loader.load('public/models/RakatOnly.obj', (object) => {
+        loader.load('public/models/RakatANDString.obj', (object) => {
             scene.add(object);
-            object.scale.set(1,3,5);
-            object.position.set(0, 0, 0);
+            object.scale.set(3,3,3);
+            object.position.set(0, -4, 0);
             console.log(object.position);
             console.log(object);
-            renderer.render(scene, camera); 
         });
         const ambientLight = new THREE.AmbientLight(0xffffff, 1);
         scene.add(ambientLight);
+
+        const animate = () => {
+            requestAnimationFrame(animate);
+            controls.update();
+            renderer.render(scene, camera);
+        };
+        animate();
+
+
     }, 
     []);
 
     return (
-        <div ref={mountRef} style={{ width: '50vw', height: '100vh' }} />
+        <div ref={mountRef} style={{ width: '50vw', height: '100vh'}} />
     );
 }
 export default RacketViewer;
