77feadd1835f9c2a78634e293fa5a5a551f819fb
shasflippedtea <shahid.anjum.shaikh@gmail.com>
2026-08-06 23:31:16 +0100

    Added each brand's flagship series ie astrox,arcsaber,nanoflare for yonex and others etc

--- customised-rackets/src/components/brand-buttons.jsx
+++ customised-rackets/src/components/brand-buttons.jsx
@@ -7,7 +7,50 @@ import apacs from '../assets/apacslogo.jpeg';
 
 export default function brandButtons() {
     const [selected, setSelected] = useState(null)
-    
+
+    if (selected === 'Yonex') {
+        return (
+        <div className="brand-grid">
+            <button classname="brand-button" onClick={() => setSelected(null)}>Go Back</button>
+            <button classname="brand-button">Astrox</button>
+            <button classname="brand-button">Arcsaber</button>
+            <button classname="brand-button">Nanoflare</button>
+        </div>
+        
+    );
+    }
+    if (selected === 'Victor') {
+        return (
+            <div className="brand-grid">
+                <button classname="brand-button" onClick={() => setSelected(null)}>Go Back</button>
+                <button classname="brand-button">Thruster</button>
+                <button classname="brand-button">Auraspeed</button>
+                <button classname="brand-button">DriveX</button>
+            </div>
+        );
+    }
+    if (selected === 'Li Ning') {
+        return (
+            <div className="brand-grid">
+                <button classname="brand-button" onClick={() => setSelected(null)}>Go Back</button>
+                <button classname="brand-button">AxForce</button>
+                <button classname="brand-button">Halbertec</button>
+                <button classname="brand-button">BladeX</button>
+            </div>
+        );
+    }
+    if (selected === 'Apacs') {
+        return (
+            <div className="brand-grid">
+                <button classname="brand-button" onClick={() => setSelected(null)}>Go Back</button>
+                <button classname="brand-button">Head-Heavy</button>
+                <button classname="brand-button">Head-Balance</button>
+                <button classname="brand-button">Head-Light</button>
+            </div>
+        );
+    }   
+
+
     return (
         <div className="brand-grid">
             <button className="brand-button" onClick={() => setSelected('Yonex')}>
