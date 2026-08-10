d28d334474360a68c55659a2da2c04c3c06d7203
shasflippedtea <shahid.anjum.shaikh@gmail.com>
2026-08-10 22:33:46 +0100

    Added the rest of the models.

--- customised-rackets/src/components/brand-buttons.jsx
+++ customised-rackets/src/components/brand-buttons.jsx
@@ -16,19 +16,29 @@ export default function brandButtons() {
             Nanoflare: ['Nanoflare 1000z', 'Nanoflare 800 Pro', 'Nanoflare 700 Pro']
         },
         Victor: {
-            Thruster: ['Thruster Ryuga'],
-            Auraspeed: [],
-            DriveX: [],
+            Thruster: ['Ryuga I', 'Ryuga II', 'Ryuga Metallic'],
+            Auraspeed: ['A99', 'A100X', 'A90K Metallic', ],
+            DriveX: ['DX12', 'DX10 Metallic', 'DX 9X'],
 
+        },
+        LiNing:  {
+            AxForce: ['AxF100', 'AxF90', 'AxF80', 'AxF70','AxF60', 'Cannon'],
+            Halbertec: ['9000','8000', '6000', '5000'],
+            BladeX: ['900 Max', '800 ', '700', ],
+        },
+        Apacs: {
+            HeadHeavy: [],
+            HeadBalance: [],
+            HeadLight: [],
         }
     }
 
     if (selected && selectedSeries) {
         return(
-            <div classname= "series-grid">
+            <div className= "series-grids">
             <button className="series-button" onClick={() => setSelectedSeries(null)}>Go Back</button>
             {brands[selected][selectedSeries].map((racket) => (
-                <button classname= "series-button" key={racket}> {racket} </button>
+                <button className= "series-button" key={racket}> {racket} </button>
             ))}
             </div>
         );
@@ -40,7 +50,6 @@ export default function brandButtons() {
             <button className="series-button" onClick={() => { setSelected(null); setSelectedSeries(null); }}>Go Back</button>
             {Object.keys(brands[selected]).map((seriesName) => (
             <button className="series-button" key={seriesName} onClick={() => setSelectedSeries(seriesName)}> {seriesName} </button>
-
         ))}
         
         </div>
@@ -48,90 +57,6 @@ export default function brandButtons() {
     );
 
 }
-
-    // YONEX RACKET SERIES 
-    // Astrox Series
-    if (selected === 'Yonex' && selectedSeries === 'Astrox') {
-        return (
-            <div className="series-grids">
-                <button className="series-button" onClick={() => setSelectedSeries(null)}>Go Back</button>
-                <button className="series-button">Astrox 99 Pro</button>
-                <button className="series-button">Astrox 88D Pro</button>
-                <button className="series-button">Astrox 77</button>
-            </div>
-        );
-    }
-
-    // Arcsaber Series
-    if (selected === 'Yonex' && selectedSeries === 'Arcsaber') {
-        return (
-            <div className="series-grids">
-                <button className="series-button" onClick={() => setSelectedSeries(null)}>Go Back</button>
-                <button className="series-button">Arcsaber 11 Pro</button>
-                <button className="series-button">Arcsaber 7 Pro</button>
-            </div>
-        );
-    }
-
-    // Nanoflare Series
-    if (selected === 'Yonex' && selectedSeries === 'Nanoflare') {
-        return (
-            <div className="series-grids">
-                <button className="series-button" onClick={() => setSelectedSeries(null)}>Go Back</button>
-                <button className="series-button">Nanoflare 1000Z </button>
-                <button className="series-button">Nanoflare 800 Pro</button>
-                <button className="series-button">Nanoflare 700 Pro</button>
-            </div>
-        );
-    }
-
-
-    // VICTOR RACKET SERIES
-
-
-
-    //Brand Buttons
-    if (selected === 'Yonex') {
-        return (
-        <div className="series-grids">
-            <button className="series-button" onClick={() => setSelected(null) }>Go Back</button>
-            <button className="series-button" onClick={() => setSelectedSeries('Astrox')}>Astrox</button>
-            <button className="series-button" onClick={() => setSelectedSeries('Arcsaber')}>Arcsaber</button>
-            <button className="series-button" onClick={() => setSelectedSeries('Nanoflare')}>Nanoflare</button>
-        </div>
-        
-    );
-    }
-    if (selected === 'Victor') {
-        return (
-            <div className="series-grids">
-                <button className="series-button" onClick={() => setSelected(null)}>Go Back</button>
-                <button className="series-button" onClick={() => setSelectedSeries('Thruster')}>Thruster</button>
-                <button className="series-button" onClick={() => setSelectedSeries('Auraspeed')}>Auraspeed</button>
-                <button className="series-button" onClick={() => setSelectedSeries('DriveX')}>DriveX</button>
-            </div>
-        );
-    }
-    if (selected === 'Li Ning') {
-        return (
-            <div className="series-grids">
-                <button className="series-button" onClick={() => setSelected(null)}>Go Back</button>
-                <button className="series-button" onClick={() => setSelectedSeries('AxForce')}>AxForce</button>
-                <button className="series-button" onClick={() => setSelectedSeries('Halbertec')}>Halbertec</button>
-                <button className="series-button" onClick={() => setSelectedSeries('BladeX')}>BladeX</button>
-            </div>
-        );
-    }
-    if (selected === 'Apacs') {
-        return (
-            <div className="series-grids">
-                <button className="series-button" onClick={() => setSelected(null)}>Go Back</button>
-                <button className="series-button" onClick={() => setSelectedSeries('Head-Heavy')}>Head-Heavy</button>
-                <button className="series-button" onClick={() => setSelectedSeries('Head-Balance')}>Head-Balance</button>
-                <button className="series-button" onClick={() => setSelectedSeries('Head-Light')}>Head-Light</button>
-            </div>
-        );
-    }   
 
 
     // Branded Logo Buttons
@@ -144,7 +69,7 @@ export default function brandButtons() {
             <button className="brand-button" onClick={() => setSelected('Victor')}>
                 <img src={victor} alt="Victor" className={selected === 'Victor' ? 'enlarged' : ''} />
             </button>
-            <button className="brand-button" onClick={() => setSelected('Li Ning')}>
+            <button className="brand-button" onClick={() => setSelected('LiNing')}>
                 <img src={lining} alt="Li Ning" className={selected === 'Li Ning' ? 'enlarged' : ''} />
             </button>
             <button className="brand-button" onClick={() => setSelected('Apacs')}>
